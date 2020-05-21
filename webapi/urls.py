64921504c016128b820f5988b55bdcc44e3dbee2
Anthony Noakes <asn.noakes@gmail.com>
2020-05-21 12:34:07 -0700

    API support and reading of body

--- webapi/urls.py
+++ webapi/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
 
+from . import views
+
 admin.autodiscover()
 
-import webapi.views
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
-    path("", webapi.views.index, name="index"),
+    path("", views.index, name="index"),
 ]
--- webapi/views.py
+++ webapi/views.py
@@ -1,14 +1,36 @@
 import requests
+import json
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # from .models import Greeting
 
 # Create your views here.
+# resquest: https://docs.djangoproject.com/en/3.0/ref/request-response/
+@csrf_exempt
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    print(request.get_host())
+    # print(request.body)
+
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    content = body['content']
+
+    return HttpResponse('<pre>' + content + '</pre>')
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
+
+def mock(request, test):
+    print(test)
+    # query = requests.utils.urlparse(request).query
+    # params = dict(x.split('=') for x in query.split('&'))
+
+    #print(params)
+    print("request")
+
+    return HttpResponse(request)
