d0d843369d2d52a7b56fbb3696002d59e435710d
Anthony Noakes <asn.noakes@gmail.com>
2020-05-21 14:12:53 -0700

    remove unused referecnes

--- webapi/sudoku_nonsense.py
+++ webapi/sudoku_nonsense.py
@@ -2,7 +2,6 @@ import cv2
 import pytesseract
 import numpy as np
 import matplotlib.pyplot as plt
-import base64
 
 # pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
